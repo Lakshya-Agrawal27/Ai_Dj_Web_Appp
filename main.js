560543cd3c20694913fa11782b71a984600984be
Lakshya-Agrawal27 <85303872+Lakshya-Agrawal27@users.noreply.github.com>
2021-12-10 13:36:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-darkside = "";
-harry_potter = "";
+song = ""
 leftwristX = 0;
 leftwristY = 0;
 rightwristX = 0;
 rightwristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function setup()
 {
@@ -24,16 +25,51 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#0000FF");
+    stroke("#0000FF");
+
+    if(scoreRightWrist > 0.01){
+
+        circle(rightwristX, rightwristY, 20);
+
+    if(rightwristY >0 && rightwristY <= 100){
+        document.getElementById("speed").innerHTML = "$peed: 0.5x";
+        song.rate(0.5);
+    } else if (rightwristY > 100 && rightwristY <= 200){
+        document.getElementById("speed").innerHTML = "$peed: 1.0x";
+        song.rate(1);
+    } else if(rightwristY > 200 && rightwristY <= 300){
+        document.getElementById("speed").innerHTML = "$peed: 1.5x";
+        song.rate(1.5);
+    } else if(rightwristY > 300 && rightwristY <= 400){
+        document.getElementById("speed").innerHTML = "$peed: 2.0";
+        song.rate(2);
+    } else if(rightwristY > 400 && rightwristY <= 500){
+        document.getElementById("speed").innerHTML = "$peed: 2.5";
+        song.rate(2.5);
+    }
+}
+
+    if(scoreLeftWrist > 0.001)
+    {
+    circle(leftwristX, leftwristY, 20);
+    InNumberleftWristY = Number(leftwristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume ="+volume;
+    song.setVolume(volume);
+    }
 }
 
 function preload(){
-    darkside = loadSound("darkside.mp3");
+    song = loadSound("music.mp3");
 }
 
 function play(){
-    darkside.play()
-    darkside.setVolume(1);
-    darkside.rate(1);
+    song.play()
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function gotPoses(results)
@@ -41,6 +77,10 @@ function gotPoses(results)
      if(results.length > 0)
      {
          console.log(results);
+         scoreLeftWrist = results[0].pose.keypoints[9].score;
+         scoreRightWrist = results[0].pose.keypoints[10].score;
+         console.log("scoreLeftWrist = " + scoreLeftWrist);
+         console.log("scoreightWrist = " + scoreRightWrist);
         
          leftwristX = results[0].pose.leftWrist.x;
          leftwristY = results[0].pose.leftWrist.y;
